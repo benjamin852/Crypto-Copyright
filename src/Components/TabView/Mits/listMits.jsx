dbb1448682038b452872a1c3a4429eeac6365fdc
Ben Weinberg <weinbergben33@gmail.com>
2019-12-03 17:07:29 -0500

    working picture

--- src/Components/TabView/Mits/listMits.jsx
+++ src/Components/TabView/Mits/listMits.jsx
@@ -16,97 +16,17 @@ import { getAvatar } from "../../../BlockchainLogic/Faucet";
 import { getMitsAction } from "../../../Actions/MitGeneration";
 import { updateItem, getItem } from "../../../utils/idb";
 
-<<<<<<< HEAD
-const useStyles = makeStyles(theme => ({
-  // card: {
-  //   maxWidth: 345
-  // },
-
-  media: {
-    height: 0,
-    paddingTop: "56.25%" // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: "rotate(180deg)"
-  },
-  avatar: {
-    backgroundColor: red[500]
-=======
 class ListMits extends Component {
   async componentDidMount() {
     let { avatar } = await getItem("accountInfo");
-    let avatarInfo = await getAvatar(avatar);
+    let avatarInfo = await getAvatar(await avatar);
     console.log(await avatarInfo);
     let address = await avatarInfo.address;
     let mits = await getMits([await address]);
     await updateItem("mits", mits);
     this.props.getMitsAction(mits);
->>>>>>> 704f78290724dec5cbc3027089c599953343bd45
   }
 
-<<<<<<< HEAD
-const ListMits = props => {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-  React.useEffect(() => {
-    console.log(props.mitSymbol);
-  }, []);
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-  return (
-    <div>
-      {props.mits ? (
-        props.mits.map(mit => (
-          <Card key={mit.symbol} className={classes.card}>
-            <CardHeader
-              avatar={
-                <Avatar
-                  alt="default metaverse mit icon"
-                  src="https://explorer-testnet.mvs.org/img/assets/default_mit.png"
-                  aria-label="recipe"
-                  className={classes.avatar}
-                />
-              }
-              title={mit.content}
-              subheader={`Owned By: ${mit.owner}`}
-            />
-            {/* <CardMedia
-        className={classes.media}
-        image="/static/images/cards/paella.jpg"
-        title="Paella dish"
-      /> */}
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {mit.symbol}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              ></IconButton>
-            </CardActions>
-          </Card>
-        ))
-      ) : (
-        <h4>Please Create some MITS</h4>
-      )}
-    </div>
-  );
-};
-=======
   render() {
     return (
       <div>
@@ -153,7 +73,6 @@ const ListMits = props => {
     );
   }
 }
->>>>>>> 704f78290724dec5cbc3027089c599953343bd45
 
 const mapStateToProps = state => ({
   mits: state.ProveitReducer.mits,
